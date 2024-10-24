5c33af287d5ff19236f888855833ab982cf96b4e
1103537 <118410718+1103537@users.noreply.github.com>
2024-10-24 18:44:14 +0800

    Create -	question_1.cpp

---
+++ -	question_1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertAtBottom(stack<int>& s, int element) {
+
+    if (s.empty()) {
+        s.push(element);
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+    insertAtBottom(s, element);
+
+    s.push(topElement);
+}
+
+void reverseStack(stack<int>& s) {
+
+    if (s.empty()) {
+        return;
+    }
+
+    int topElement = s.top();
+    s.pop();
+    reverseStack(s);
+
+    insertAtBottom(s, topElement);
+}
+void printStack(stack<int> s) {
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    stack<int> s;
+
+    s.push(28);
+    s.push(29);
+    s.push(30);
+    s.push(31);
+
+    cout << "Original Stack (from top to bottom): ";
+    printStack(s);
+    reverseStack(s);
+
+    cout << "Reversed Stack (from top to bottom): ";
+    printStack(s);
+
+    return 0;
+}
